d23610811579253817d132850c2a5f515ea59954
Raliii <r.ilieva-student@azbuki-school.bg>
2022-06-09 11:19:19 +0300

    background

--- WAP.py
+++ WAP.py
@@ -3,11 +3,10 @@ import winsound
 import time
 import datetime
 
-
 def alarm(set_alarm_timer):
 
     while True:
-        time.sleep(1)
+        time.sleep(60)
         current_time = datetime.datetime.now()
         now = current_time.strftime("%H:%M:%S")
         date = current_time.strftime("%d/%m/%Y")
@@ -20,23 +19,23 @@ def alarm(set_alarm_timer):
             break
 
 def actual_time():
-    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+    set_alarm_timer = f"{hour.get()}:{min.get()}"
     alarm(set_alarm_timer)
 
 clock = Tk()
 clock.title("WAP")
 clock.geometry("700x500")
-time_format = Label(clock, text = " Enter time in 24 hour format! ", fg = "white", bg = "black", font = "Arial").place(x = 225, y = 350)
-addTime = Label(clock, text = " Hour:     " + "Min:     " + "Sec:     ", font = 60).place(x = 250, y = 160)
-setYourAlarm = Label(clock, text = " When to drink water: ", fg = "black", relief = "sunken", font = ("Arial", 28, "bold")).place(x = 165, y = 50)
+clock.config(bg='#8cd1fe')
+
+time_format = Label(clock, text = " Enter time in 24 hour format! ", fg = "white", bg = "black", font = ("Arial", 12, "italic")).place(x = 250, y = 300)
+addTime = Label(clock, text = " Hour:" + "      " + "Min: ", font = ("Arial", 14, "bold"), bg = "#8cd1fe").place(x = 280, y = 160)
+setYourAlarm = Label(clock, text = " When to drink water: ", fg = "black", relief = "sunken", font = ("Arial", 28, "bold")).place(x = 160, y = 60)
 
 hour = StringVar()
 min = StringVar()
-sec = StringVar()
 
-hourTime= Entry(clock, textvariable = hour, bg = "white", width = 10).place(x = 250, y = 190)
-minTime= Entry(clock, textvariable = min, bg = "white", width = 10).place(x = 320, y = 190)
-secTime = Entry(clock, textvariable = sec, bg = "white", width = 10).place(x = 390, y = 190)
+hourTime= Entry(clock, textvariable = hour, bg = "white", width = 11).place(x = 280, y = 190)
+minTime= Entry(clock, textvariable = min, bg = "white", width = 11).place(x = 360, y = 190)
 
-submit = Button(clock, text = " Set Alarm ", fg = "black", width = 10, command = actual_time).place(x = 312, y = 250)
+submit = Button(clock, text = " Set Alarm ", fg = "black", width = 11, font = ("Arial", 12), command = actual_time).place(x = 300, y = 240)
 clock.mainloop()
