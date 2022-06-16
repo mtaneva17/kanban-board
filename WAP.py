8622bb66a40d3a9d8e50fd1541747a10e7d799b3
Raliii <100564418+Raliii@users.noreply.github.com>
2022-06-17 02:34:29 +0300

    tablica

--- WAP.py
+++ WAP.py
@@ -1,8 +1,10 @@
+from timeit import repeat
 from tkinter import *
 import winsound
 import schedule
 import time
 import datetime
+from tkinter import Spinbox
 
 
 def alarm(set_alarm_timer):
@@ -17,9 +19,14 @@ def alarm(set_alarm_timer):
 
         if now == set_alarm_timer:
             print("Time To Drink Water!")
-            winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
+            winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
 
-  
+
+def actual_time():
+    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+    alarm(set_alarm_timer)
+    
+
 def job():
     schedule.every(45).minutes.do(job)
     while True:
@@ -27,29 +34,126 @@ def job():
         time.sleep(1)
 
 
-def actual_time():
-    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
-    alarm(set_alarm_timer)
-
-
 clock = Tk()
 clock.title("WAP")
-clock.geometry("700x500")
+clock.geometry("700x600")
 clock.config(bg='#8cd1fe')
 
-time_format = Label(clock, text = " Enter time in 24 hour format! ", fg = "white", bg = "black", font = ("Arial", 12, "italic")).place(x = 250, y = 300)
-addTime = Label(clock, text = " Hour: " + "      " + "Min: " + "      " + "Sec: ", font = ("Arial", 14, "bold"), bg = "#8cd1fe").place(x = 240, y = 160)
-setYourAlarm = Label(clock, text = " When to drink water: ", fg = "black", relief = "sunken", font = ("Arial", 28, "bold")).place(x = 160, y = 60)
+time_format = Label(
+    clock, 
+    text = " Enter time in 24 hour format! ", 
+    fg = "white", bg = "#000000", font = ("Arial", 12, "italic")
+    ).place(x = 242, y = 300)
+
+addHr = Label(
+    clock, 
+    text = " Hour: ",
+    font = ("Arial", 18, "bold"), 
+    bg = "#8cd1fe"
+    ).place(x = 228, y = 158)
+addMn = Label(
+    clock, 
+    text = " Min: ", 
+    font = ("Arial", 18, "bold"), 
+    bg = "#8cd1fe"
+    ).place(x = 314, y = 158)
+addSc = Label(
+    clock, 
+    text = " Sec: ", 
+    font = ("Arial", 18, "bold"), 
+    bg = "#8cd1fe"
+    ).place(x = 392, y = 158)
+
+setYourAlarm = Label(
+    clock, 
+    text = " When to drink water: ", 
+    bg = '#EEFCFF', 
+    fg = "black", 
+    relief = "sunken", 
+    font = ("Arial", 32, "bold")
+    ).place(x = 125, y = 60)
+
 
 hour = StringVar()
 min = StringVar()
 sec = StringVar()
 
-hourTime= Entry(clock, textvariable = hour, bg = "white", width = 11).place(x = 240, y = 190)
-minTime= Entry(clock, textvariable = min, bg = "white", width = 11).place(x = 320, y = 190)
-secTime = Entry(clock, textvariable = sec, bg = "white", width = 11).place(x = 400, y = 190)
 
-submit = Button(clock, text = " Set Alarm ", fg = "black", width = 11, font = ("Arial", 12), command = actual_time).place(x = 300, y = 240)
+hourTime = Entry(
+    clock, 
+    textvariable = hour, 
+    bg = "#EEFCFF", 
+    width = 6,
+    font= ("Arial", 16),
+    ).place(x = 230, y = 190)
+
+minTime = Entry(
+    clock, 
+    textvariable = min, 
+    bg = "#EEFCFF", 
+    width = 6,
+    font= ("Arial", 16),
+    ).place(x = 310, y = 190)
+
+secTime = Entry(
+    clock, 
+    textvariable = sec, 
+    bg = "#EEFCFF", 
+    width = 6,
+    font= ("Arial", 16),
+    ).place(x = 390, y = 190)
+
+
+submit = Button(
+    clock, 
+    text = " Set Alarm ", 
+    fg = "black", 
+    width = 12, 
+    font = ("Arial", 14), 
+    command = actual_time
+    ).place(x = 280, y = 240)
+
+
+checker = Label(
+    clock, 
+    relief = "sunken", 
+    bg = "#EEFCFF",
+    text = " How much did you drink? ", 
+    fg = "#000000", 
+    font = ("Arial", 20, "bold")
+    ).place(x = 170, y = 380)
+
+sb = Spinbox(
+    clock,
+    bg = "#EEFCFF",
+    width = 8,
+    from_ = 0,
+    to = 1000,
+    increment = 50,
+    format = '%8.0f',
+    font = ("Arial", 18)
+).place(x = 268, y = 435)
+
+
+mil = Label(
+    clock, 
+    text = " Milliliters: ", 
+    bg = "#EEFCFF",
+    relief = "ridge", 
+    font = ("Arial", 18), 
+    ).place(x = 145, y = 434)
+
+sub_mil = Button(
+    clock, 
+    # bg = ,
+    text = " Checked ✔ ", 
+    fg = "#000000", 
+    width = 10, 
+    height = 0,
+    font = ("Arial", 12), 
+    command = ()
+    ).place(x = 445, y = 434)
+
 clock.mainloop()
 
 
